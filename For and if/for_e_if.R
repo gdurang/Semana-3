c3092afce802529c9ae393d8a95360ca95e78d58
gdurang <33270927+gdurang@users.noreply.github.com>
2022-10-04 20:32:18 -0300

    en clases

--- For and if/for_e_if.R
+++ For and if/for_e_if.R
@@ -3,13 +3,13 @@ install.packages("stringr")
 library(stringr)
 
 #Constate y variables
-ean13<-"8412345678905"
+ean13<-"4100016512002"
 eani<-integer(13)
 sumai<-0
 sumap<-0
 
 #creación de vector de entero
-eani<-as.numeric(unlist(str_split(ean13, "")))
+eani<-as.numeric(unlist(str_split(ean13,"")))
 
 #Cálculo de código de control 
 for(i in 1:12)
@@ -19,4 +19,4 @@ for(i in 1:12)
   if((i%%2)==0)
     {sumap<-sumap+eani[i]} 
 }
-print((sumai+sumap*3)%%10)
+print(10-(sumai*1+sumap*3)%%10)
--- For and if/if.R
+++ For and if/if.R
@@ -1,23 +1,27 @@
-# creación de la función f
+# creación de la función f a trozos
+M<-100
 f <- function(x) 
 {
   if (x<0) 
     {
-    5
+    r<-5
     } 
     else 
       {
       if (x<=10) 
         {
-        x*10+5
+        r<-x*10+5
         } 
         else 
           {
-          105
+          r<-105
           }
       }
+M<<-r
+return(r)
 }
 # Prueba de la función
 f(-10)
 f(5)
 f(15)
+M
